f87d9144e46ee408b55d5a50778bf6779fa0108e
Eugene Dyakonov <eugene.dyakonov@gmail.com>
2019-08-03 00:40:37 +0400

    Initial Commit

---
+++ simple-calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-calculator
+//
+//  Created by Евгений Дьяконов on 03/08/2019.
+//  Copyright © 2019 Евгений Дьяконов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
